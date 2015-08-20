1c408b9a31a5aacf4a81c88206109cc641c95769
eamoncaddigan <eamon.caddigan@gmail.com>
2015-08-19 22:45:49 -0400

    Filtering bad Ps.

--- antivax-attitudes.R
+++ antivax-attitudes.R
@@ -6,3 +6,11 @@ library(ggplot2)
 
 # Generates warnings, but only for the Ps who didn't do day 2
 expData <- read_excel("Vacc_HPHH_publicDataset.xlsx", sheet = 2)
+
+# Just exclude Ps who didn't do day 2 and failed the attention checks
+expData.clean <- expData %>%
+  filter(!is.na(StartDate_day2),
+         `AttentionCheck_PostTest (if = 4 then include)` == 4,
+         `AttentionChecks_Sum(include if = 4)` == 4,
+         Paid_Attention == 1)
+
